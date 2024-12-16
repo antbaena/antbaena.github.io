9673eb69eb6acee04591e6735c9bdfac321c32f2
Antonio Cañete Baena <70489950+antbaena@users.noreply.github.com>
2024-12-16 22:30:44 +0100

    add reume.pdf

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -75,7 +75,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
+      href: '/public/resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
