0669047566c179eff977f414ab84576beb0b64d6
antBaena <antoniocanetebaena1234@gmail.com>
2025-07-23 01:09:31 +0200

    Updates

---
+++ _next/static/XfxVIgWwodINfZlfgTjVy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/807-103205fccb2f8059.js","static/chunks/pages/index-76d657fdf2d32753.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/xbORngxWj6lT0iBqHINkq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/807-54e527f74fad29d8.js","static/chunks/pages/index-e785f8c741139957.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
