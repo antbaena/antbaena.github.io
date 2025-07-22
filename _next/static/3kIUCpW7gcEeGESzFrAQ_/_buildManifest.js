669d3a9df0833719e669f1688f642513c4dc5c5a
antBaena <antoniocanetebaena1234@gmail.com>
2025-07-23 01:16:53 +0200

    Updates

---
+++ _next/static/3kIUCpW7gcEeGESzFrAQ_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/807-103205fccb2f8059.js","static/chunks/pages/index-80980e4c96bad273.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
